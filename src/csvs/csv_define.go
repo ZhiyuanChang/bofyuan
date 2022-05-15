7ec779ea078031d470602536eec36e0e7195b63d
changpingdengdengdengdeng <changzhiyuan@outlook.com>
2022-05-15 23:25:53 +0800

    Create:drive of redis

--- src/csvs/csv_define.go
+++ src/csvs/csv_define.go
@@ -8,4 +8,7 @@ const (
 	REDUCE_WORLD_LEVEL_COOL_TIME = 24 * 3600 * time.Second
 	LOGIC_FALSE                  = 0
 	LOGIC_TRUE                   = 1
+	SHOW_SIZE                    = 9
+	ADD_ROLE_TIME_NORMAL_MIN     = 2
+	ADD_ROLE_TIME_NORMAL_MAX     = 9
 )
